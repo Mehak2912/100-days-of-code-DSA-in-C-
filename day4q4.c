8df5d22b9e33d0e0c56b4b879dc68729610067f4
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2026-02-04 22:43:27 +0530

    Implement array reversal in C

---
+++ day4q4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void reverseArray(int arr[], int n) {
+    int left = 0;
+    int right = n - 1;
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
+int main() {
+    int arr[100], n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    reverseArray(arr, n);
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
