ec8a7aaec45904138b17cda7c1c1018f400558f2
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-02-12 23:29:38 +0530

    Create day12q12.c

---
+++ day12q12.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int m, n;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    if (m != n)
+    {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+    int matrix[m][n];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int isSymmetric = 1;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] != matrix[j][i])
+            {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if (!isSymmetric)
+            break;
+    }
+    if (isSymmetric)
+    {
+        printf("Symmetric Matrix");
+    }
+    else
+    {
+        printf("Not a Symmetric Matrix");
+    }
+    return 0;
+}
