621507afd36cb301ff8ec35c5ce790ec24c12c1c
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-03-21 13:56:31 +0530

    Create day49q49.c

---
+++ day49q49.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int key) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = key;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+struct Node* insert(struct Node* root, int key) {
+    // If tree is empty
+    if (root == NULL) {
+        return createNode(key);
+    }
+    if (key < root->data) {
+        root->left = insert(root->left, key);
+    } 
+    else if (key > root->data) {
+        root->right = insert(root->right, key);
+    }
+    return root;
+}
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+int main() {
+    struct Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+    printf("Inorder Traversal: ");
+    inorder(root);
+    return 0;
+}
