30a55ba50c547d16b9d7142056aa35c92051a959
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-04-13 15:21:14 +0530

    Add first repeating character finder
    Implement a program to find the first repeating character in a string.

---
+++ day72q72.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[1000];
+    scanf("%s", s);
+    int freq[26] = {0};
+    for (int i = 0; i < strlen(s); i++) {
+        int index = s[i] - 'a';
+        if (freq[index] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+        freq[index] = 1;
+    }
+    printf("-1\n");
+    return 0;
+}
