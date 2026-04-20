15fb2846ae0189d8098c459c536255a4d256c3ad
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-04-20 14:58:39 +0530

    Implement DFS for checking graph connectivity

---
+++ day77q77.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int adj[MAX][MAX], visited[MAX];
+void dfs(int node, int n) {
+    visited[node] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+    dfs(1, n);
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+    printf("CONNECTED\n");
+    return 0;
+}
